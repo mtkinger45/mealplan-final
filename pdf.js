223f1e98a50ff04496b0c67922f7c2308a7ab050
mtkinger45 <matt@theoilkings.com>
2025-05-03 14:15:31 -0500

    Add files via upload

--- pdf.js
+++ pdf.js
@@ -1,20 +1,10 @@
-// pdf.js
 import PDFDocument from 'pdfkit';
-import { Buffer } from 'node:buffer';
 import getStream from 'get-stream';
 
 export async function createPdfFromText(text) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const doc = new PDFDocument();
-      const stream = doc.pipe(getStream.buffer());
-      doc.fontSize(12).text(text || 'No content provided.', { align: 'left' });
-      doc.end();
-      const buffer = await stream;
-      const base64 = buffer.toString('base64');
-      resolve(base64);
-    } catch (err) {
-      reject(err);
-    }
-  });
+  const doc = new PDFDocument();
+  doc.text(text);
+  doc.end();
+  const buffer = await getStream.buffer(doc);
+  return 'data:application/pdf;base64,' + buffer.toString('base64');
 }
--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
