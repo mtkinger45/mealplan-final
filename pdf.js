07aabaae6c88608ddeecc5752e05755fe1938b2a
mtkinger45 <matt@theoilkings.com>
2025-05-03 23:08:14 -0500

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,6 +1,9 @@
-import PDFDocument from 'pdfkit';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+// pdf.js
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import PDFDocument from 'pdfkit';
+import { PassThrough } from 'stream';
+import { Readable } from 'stream';
 
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
@@ -11,47 +14,46 @@ const s3 = new S3Client({
 });
 
 export async function createPdfFromText(text) {
-  return new Promise((resolve, reject) => {
-    const doc = new PDFDocument({ margin: 40 });
-    const buffers = [];
+  console.log('[createPdfFromText] Generating PDF for content...');
+  const doc = new PDFDocument();
+  const buffers = [];
 
-    const lines = text.split('\n');
-    lines.forEach((line, idx) => {
-      // Bold text between <b>...</b>
-      if (line.match(/^<b>(.*?)<\/b>$/)) {
-        const content = line.replace(/<\/?b>/g, '');
-        doc.font('Helvetica-Bold').fontSize(14).text(content);
-      } else if (line.match(/^_.*_$/)) {
-        const content = line.replace(/^_(.*?)_$/, '$1');
-        doc.font('Helvetica-Oblique').fontSize(12).text(content);
-      } else {
-        doc.font('Helvetica').fontSize(12).text(line);
-      }
-      if (idx < lines.length - 1) doc.moveDown(0.5);
+  doc.on('data', buffers.push.bind(buffers));
+  doc.on('end', () => {
+    console.log('[createPdfFromText] PDF generation complete.');
+  });
+
+  doc.text(text);
+  doc.end();
+
+  return new Promise((resolve) => {
+    doc.on('end', () => {
+      const pdfBuffer = Buffer.concat(buffers);
+      resolve(pdfBuffer);
     });
-
-    doc.on('data', buffers.push.bind(buffers));
-    doc.on('end', () => resolve(Buffer.concat(buffers)));
-    doc.end();
   });
 }
 
-export async function uploadPdfToS3(buffer, key) {
+export async function uploadPdfToS3(buffer, filename) {
+  console.log(`[uploadPdfToS3] Uploading ${filename} to S3...`);
+  
   const bucketName = process.env.AWS_BUCKET_NAME;
-  const putCommand = new PutObjectCommand({
+
+  const uploadCommand = new PutObjectCommand({
     Bucket: bucketName,
-    Key: key,
+    Key: filename,
     Body: buffer,
     ContentType: 'application/pdf'
   });
 
-  await s3.send(putCommand);
+  await s3.send(uploadCommand);
 
-  const getCommand = new PutObjectCommand({
+  // Now generate a signed GET URL to allow download
+  const getCommand = new GetObjectCommand({
     Bucket: bucketName,
-    Key: key
+    Key: filename
   });
 
-  const url = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
+  const url = await getSignedUrl(s3, getCommand, { expiresIn: 3600 }); // 1 hour
   return url;
 }
