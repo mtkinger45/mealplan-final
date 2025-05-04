8d727b8c7ed9b5a635390a3972e2fade9a79c404
mtkinger45 <matt@theoilkings.com>
2025-05-03 21:51:27 -0500

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,44 +1,60 @@
-// pdf.js
-import PDFDocument from 'pdfkit';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import stream from 'stream';
-import { promisify } from 'util';
 
-const finished = promisify(stream.finished);
+// pdf.js
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import PDFDocument from 'pdfkit';
+import { PassThrough } from 'stream';
+import { Readable } from 'stream';
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
 
 export async function createPdfFromText(text) {
   console.log('[createPdfFromText] Generating PDF for content...');
   const doc = new PDFDocument();
   const buffers = [];
+
   doc.on('data', buffers.push.bind(buffers));
-  doc.on('end', () => console.log('[createPdfFromText] PDF generation complete.'));
+  doc.on('end', () => {
+    console.log('[createPdfFromText] PDF generation complete.');
+  });
+
   doc.text(text);
   doc.end();
-  await finished(doc);
-  return Buffer.concat(buffers);
+
+  return new Promise((resolve) => {
+    doc.on('end', () => {
+      const pdfBuffer = Buffer.concat(buffers);
+      resolve(pdfBuffer);
+    });
+  });
 }
 
 export async function uploadPdfToS3(buffer, filename) {
   console.log(`[uploadPdfToS3] Uploading ${filename} to S3...`);
-  const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-  });
+  
+  const bucketName = process.env.AWS_BUCKET_NAME;
 
-  const command = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME,
+  const uploadCommand = new PutObjectCommand({
+    Bucket: bucketName,
     Key: filename,
     Body: buffer,
     ContentType: 'application/pdf'
   });
 
-  await s3.send(command);
-  console.log(`[uploadPdfToS3] Upload complete, generating signed URL...`);
+  await s3.send(uploadCommand);
 
-  const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+  // Now generate a signed GET URL to allow download
+  const getCommand = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: filename
+  });
+
+  const url = await getSignedUrl(s3, getCommand, { expiresIn: 3600 }); // 1 hour
   return url;
 }
