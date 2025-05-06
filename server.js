0966cdbd467763c3d16cdf72821652c97f92b317
mtkinger45 <matt@theoilkings.com>
2025-05-06 18:46:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.get('/api/pdf/mealplan', async (req, res) => {
   try {
     const cleanedText = latestPlan.mealPlan
       .replace(/^\*\*Meal Plan\*\*\n?/i, '')
-      .replace(/^\*\*([^\n]+?)\*\*/gm, (_, day) => `<b>${day}</b>`) // Bold days
+      .replace(/^\*\*([^\n]+?)\*\*/gm, (_, day) => `\n<b>${day}</b>`) // Bold days
       .replace(/^\*\s*/gm, '') // Remove asterisk from meal lines
       .replace(/^-\s*/gm, ''); // Also remove dashes if any
 
