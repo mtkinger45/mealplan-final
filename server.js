c59f1614bf4998d31c30a8e7135a2151520f46e4
mtkinger45 <matt@theoilkings.com>
2025-05-14 14:27:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -207,10 +207,9 @@ Instructions:
         filename = `${sessionId}-mealplan.pdf`;
       } else if (type === 'recipes') {
         content = cache.recipes;
+    console.log('[RECIPE PDF] Content length:', content?.length || 0);
+    console.log('[RECIPE PDF] Preview:', content?.slice(0, 500));
         filename = `${sessionId}-recipes.pdf`;
-        console.log('[RECIPE PDF] Content length:', cache.recipes?.length || 0);
-console.log('[RECIPE PDF] Preview:', cache.recipes?.slice(0, 500));
-
       } else if (type === 'shopping-list') {
         content = cache.shoppingList;
         filename = `${sessionId}-shopping.pdf`;
